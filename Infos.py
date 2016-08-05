940bda5ccbee3946a39167d126d10bad2e74224f
qiujieqiong <qiujieqiong@deepin.com>
2016-08-05 16:12:57 +0800

    Add cases 启动 搜索 操作 分类

--- Infos.py
+++ Infos.py
@@ -2,8 +2,105 @@
 #encoding:utf-8
 
 import subprocess
+from time import sleep
 import json
+from Xlib import X,display
+from dogtail.tree import *
+import pyautogui
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
+class GetInfo:
+
+	def __init__(self):
+		self.d = display.Display()
+		self.root = self.d.screen().root
+		self.root.change_attributes(event_mask = X.SubstructureNotifyMask)
+
+	def loop(self):
+		window_changed = False
+		while True:
+			event = self.d.next_event()
+			if event.type == X.MapNotify:
+				window_changed = True
+				break
+		return window_changed
+
+def exit_launcher():
+    name = get_actived_win_name()
+    if name=='dde-launcher':
+        print 'launcher opened!'
+        pyautogui.press('esc')
+
+def getDockedApps():
+	apps = subprocess.check_output(["gsettings get com.deepin.dde.dock docked-apps"],shell=True).decode().split("\n")
+	apps = [ n for n in apps if len(n.strip()) > 0]
+	apps = ''.join(apps)
+	return apps
+
+def getDesktopFiles():
+	userPath = os.path.expanduser('~')
+	absDesktopPath = userPath + '/桌面'
+	files = subprocess.check_output(["ls " +absDesktopPath],shell=True).decode().split("\n")
+	files = [ n for n in files if len(n.strip()) > 0]
+	files = ''.join(files)
+	return files
+
+def getStartupInfo():
+	userPath = os.path.expanduser('~')
+	absAutostartupPath = userPath + '/.config/deepin.eog'
+	feild = subprocess.check_output(["cat " +absAutostartupPath+"|grep hidden"],shell=True).decode().split("\n")
+	feild = [ n for n in feild if len(n.strip()) > 0]
+	feild = ''.join(feild)
+	return feild
+
+def getLauncherItems():
+	items = []
+	launcherobj = root.application(appName='dde-launcher', description='/usr/bin/dde-launcher')
+	launcheritems = launcherobj.child('all',roleName='list').children
+	for i in range(len(launcheritems)):
+		items.append(launcheritems[i].name)
+	print items
+	return items
+def getdeepinItems():
+	deepinItems = ['Deepin Cloud Print', 'Deepin Boot Maker', 'Deepin User Feedback', 'Deepin Terminal', 'Deepin Store',
+					'Deepin Music', 'Deepin Screenshot', 'Remote Assistance', 'Multitasking View', 'Show Desktop', 'Control Center']
+	return deepinItems
+
+def getLauncherMode():
+	mode = subprocess.check_output(["gsettings get com.deepin.dde.launcher display-mode"],shell=True).decode().split("\n")
+	mode = [ n for n in mode if len(n.strip()) > 0]
+	mode = ''.join(mode)
+	return mode
+
+def clickUnintallBtn():
+	launcherobj = root.application(appName='dde-launcher', description='/usr/bin/dde-launcher')
+	launcherobj.child('Confirm').click()
+
+def clickToggleBtn():
+	launcherobj = root.application(appName='dde-launcher', description='/usr/bin/dde-launcher')
+	launcherobj.child('mode-toggle-button').click()
+
+def launcherSearchDeepin():
+	pyautogui.press('winleft')
+	name = get_actived_win_name()
+	sleep(2)
+	if name == 'dde-launcher':
+		launcherobj = root.application(appName='dde-launcher', description='/usr/bin/dde-launcher')
+		launcherobj.child('search-edit').text = 'deepin'
+	else:
+		raise Exception("dde-launcher did not opened!")
+
+def imageviewer_rightKey():
+	pyautogui.press('winleft')
+	name = get_actived_win_name()
+	sleep(2)
+	if name == 'dde-launcher':
+		launcherobj = root.application(appName='dde-launcher', description='/usr/bin/dde-launcher')
+		launcherobj.child('Image Viewer').click(3)
+	else:
+		raise Exception("dde-launcher did not opened!")
 
 
 def tag(*args, **kwargs):
--- testdemo.py
+++ testdemo.py
@@ -5,15 +5,10 @@ import unittest
 import sys
 from time import sleep
 from dogtail.tree import *
+from dogtail.predicate import GenericPredicate
 from Infos import *
 import pyautogui
 
-
-def exit_launcher():
-    name = get_actived_win_name()
-    if name=='dde-launcher':
-        print 'launcher opened!'
-        pyautogui.press('esc')
 
 class My_test(unittest.TestCase):
     @classmethod
@@ -24,20 +19,21 @@ class My_test(unittest.TestCase):
     def tearDown(cls):
         pass
     @tag("57636")
-    def test_launcher_start_up1(self):
+    def test_launcher_start_up(self):
+        #startup1
         app = root.application(appName='dde-dock', description='/usr/bin/dde-dock')
         app.child('Launcher').click()
         name = get_actived_win_name()
         self.assertEqual(name,'dde-launcher')
         exit_launcher()
 
-    def test_launcher_start_up2(self):
+        #startup2
         pyautogui.press('winleft')
         name = get_actived_win_name()
         self.assertEqual(name,'dde-launcher')
         exit_launcher()
-    @tag('57646')
-    def test_launcher_start_up3(self):
+
+        #startup3
         pyautogui.moveTo(self.screenWidth/2, self.screenHeight/2)
         pyautogui.click(button='right')
         pyautogui.press('c')
@@ -50,25 +46,110 @@ class My_test(unittest.TestCase):
         name = get_actived_win_name()
         self.assertEqual(name,'dde-launcher')
         exit_launcher()
-    @tag('57649')
-    def test_launcher_start_up4(self):
+
+        #startup4
         pyautogui.hotkey('ctrl','alt','t')
-        sleep(2)
-        name = get_actived_win_name()
-        self.assertEqual(name,'Deepin Terminal')
-        pyautogui.typewrite('dde-launcher -s',interval=0.25)
-        pyautogui.press('enter')
+        if GetInfo().loop():
+            print "Deepin Terminal Opend!"
+            name = get_actived_win_name()
+            self.assertEqual(name,'Deepin Terminal')
+        else:
+            raise Exception("Deepin Terminal did not opened!")
+        sleep(1)
+        pyautogui.typewrite('dde-launcher -s\n',interval=0.25)
+        sleep(1)
         name = get_actived_win_name()
         self.assertEqual(name,'dde-launcher')
         exit_launcher()
         pyautogui.hotkey('alt','f4')
-    #@tag('57646')
 
+    @tag('57646')
+    def test_launcher_search(self):
+        launcherSearchDeepin()
+        deepinItems = getdeepinItems()
+        exit_launcher()
+        items = getLauncherItems()
+        self.assertListEqual(deepinItems,items)
 
-    #@tag('57649')
+    @tag('57649')
+    def test_launcher_operation(self):
+        #open
 
+        imageviewer_rightKey()
+        pyautogui.press('o')
+        if GetInfo().loop():
+            print "Deepin Store Opened!"
+            name = get_actived_win_name()
+            self.assertIn('Deepin Store', name)
+            if name != 'dde-desktop':
+                pyautogui.hotkey('alt','f4')
+        else:
+            raise Exception("Deepin Store did not opened!")
 
-    #@tag('57652')
+        #send to desktop
+        imageviewer_rightKey()
+        pyautogui.press(['down','down','enter'])
+        exit_launcher()
+        desktopfiles = getDesktopFiles()
+        self.assertIn('eog.desktop', desktopfiles)
+
+        #remove from desktop
+        imageviewer_rightKey()
+        pyautogui.press(['down','down','enter'])
+        exit_launcher()
+        desktopfiles = getDesktopFiles()
+        self.assertNotIn('eog.desktop', desktopfiles)
+
+        #undock
+        imageviewer_rightKey()
+        pyautogui.press(['down','down','down','enter'])
+        exit_launcher()
+        apps = getDockedApps()
+        self.assertNotIn('eog', apps)
+
+        #dock
+        imageviewer_rightKey()
+        pyautogui.press(['down','down','down','enter'])
+        exit_launcher()
+        apps = getDockedApps()
+        self.assertIn('eog', apps)
+
+        #add to startup
+        imageviewer_rightKey()
+        pyautogui.press(['down','down','down','down','enter'])
+        exit_launcher()
+        feild = getStartupInfo()
+        self.assertEqual(feild, 'Hidden=false')
+        imageviewer_rightKey()
+        pyautogui.press(['down','down','down','down','enter'])
+        exit_launcher()
+        feild = getStartupInfo()
+        self.assertEqual(feild, 'Hidden=true')
+
+        #uninstall
+        imageviewer_rightKey()
+        pyautogui.press(['down','down','down','down','down','enter'])
+        clickUnintallBtn()
+        exit_launcher()
+        if GetInfo().loop():
+            items = getLauncherItems()
+            self.assertNotIn('Image Viewer',items)
+            
+    @tag('57652')
+    def test_launcher_classify(self):
+        pyautogui.press('winleft')
+    	name = get_actived_win_name()
+    	sleep(2)
+    	if name == 'dde-launcher':
+            clickToggleBtn()
+            mode = getLauncherMode()
+            self.assertEqual('\'category\'',mode)
+            clickToggleBtn()
+            mode = getLauncherMode()
+            self.assertEqual('\'free\'',mode)
+            exit_launcher()
+        else:
+            raise Exception('launcher Did not opened!')
 
 
     #@tag('57720')
@@ -86,17 +167,19 @@ def suite():
     if '57636' in casesID:
         suite.addTest(My_test('test_launcher_start_up'))
     if '57646' in casesID:
-        suite.addTest(My_test('test_launcher_start_up3'))
+        suite.addTest(My_test('test_launcher_search'))
     if '57649' in casesID:
-        suite.addTest(My_test('test_launcher_start_up4'))
+        suite.addTest(My_test('test_launcher_operation'))
     if '57652' in casesID:
-        suite.addTest(My_test('test_method4'))
+        suite.addTest(My_test('test_launcher_classify'))
+    '''
     if '57720' in casesID:
-        suite.addTest(My_test('test_method5'))
+        suite.addTest(My_test('test_launcher_classify'))
     if '57722' in casesID:
-        suite.addTest(My_test('test_method6'))
+        suite.addTest(My_test('test_launcher_search'))
     if '57724' in casesID:
         suite.addTest(My_test('test_method7'))
+    '''
     return suite
 
 if __name__ == '__main__':
